c3e915a66dc5c2e7c0742d7c647f2db6f922428a
Doug Mathew <dmathew@office.consulnet.com>
2019-02-04 11:33:37 -0500

    a cool way to generate avatar colors based on name

--- src/components/People/PersonIcon.js
+++ src/components/People/PersonIcon.js
@@ -1,6 +1,29 @@
 import React from "react";
 import Avatar from "@material-ui/core/Avatar";
 
+function hashCode(str) {
+    // java String#hashCode
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    return hash;
+}
+
+function intToRGB(i) {
+    var c = (i & 0x00ffffff).toString(16).toUpperCase();
+
+    return "00000".substring(0, 6 - c.length) + c;
+}
+function getColorByBgColor(bgColor) {
+    if (!bgColor) {
+        return "";
+    }
+    return parseInt(bgColor.replace("#", ""), 16) > 0xffffff / 2
+        ? "#000"
+        : "#fff";
+}
+
 const PersonIcon = props => {
     let initals;
     const { name } = props;
@@ -10,7 +33,21 @@ const PersonIcon = props => {
             initals += name[1];
         }
     }
-    return <Avatar className="person-icon">{initals}</Avatar>;
+
+    console.log(hashCode(props.name));
+
+    const bgColor = "#" + intToRGB(hashCode(props.name));
+
+    const iconStyles = {
+        backgroundColor: bgColor,
+        color: getColorByBgColor(bgColor)
+    };
+
+    return (
+        <Avatar className="person-icon" style={iconStyles}>
+            {initals}
+        </Avatar>
+    );
 };
 
 export default PersonIcon;
